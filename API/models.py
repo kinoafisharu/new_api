985aa98d20cfa3fe301a5c04fa588fb6cc0dd224
Иванов и Ко <32426965+kinoafisharu@users.noreply.github.com>
2020-05-15 17:13:03 +0300

    1 vers

--- API/models.py
+++ API/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+# справочники: страны, годы, жанры, источники киноданных, возр.ограничения, языки, профессии
+# объекты: названия локализованные, названия международные, названия оригинальные, фильмы, организации, люди
+# процессы: оценки, мнения, подписки, сеансы
 
-# Create your models here.
+class Film(models.Model):
+    id = models.IntegerField(primary_key=True, db_column='id')
+    title_local = models.ForeignKey(FilmNamesLocal)
+    title_int = models.ForeignKey(FilmNamesInt)
+    title_orig = models.ForeignKey(FilmNamesOrig)
+    country = models.ForeignKey(Country)
