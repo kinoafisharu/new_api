7206f780cf8693ff22827b94beff0d81ea4af968
Encouse <54453362+Encouse@users.noreply.github.com>
2020-05-15 17:47:30 +0300

    Add files via upload

--- API/models.py
+++ API/models.py
@@ -1,11 +1,13 @@
+
+
 from django.db import models
 # справочники: страны, годы, жанры, источники киноданных, возр.ограничения, языки, профессии
 # объекты: названия локализованные, названия международные, названия оригинальные, фильмы, организации, люди
 # процессы: оценки, мнения, подписки, сеансы
 
-class Film(models.Model):
-    id = models.IntegerField(primary_key=True, db_column='id')
-    title_local = models.ForeignKey(FilmNamesLocal)
-    title_int = models.ForeignKey(FilmNamesInt)
-    title_orig = models.ForeignKey(FilmNamesOrig)
-    country = models.ForeignKey(Country)
+#class Film(models.Model):
+#    id = models.IntegerField(primary_key=True, db_column='id')
+#    title_local = models.ForeignKey(FilmNamesLocal)
+#    title_int = models.ForeignKey(FilmNamesInt)
+#    title_orig = models.ForeignKey(FilmNamesOrig)
+#    country = models.ForeignKey(Country)
