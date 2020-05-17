5d1b61999c5e788d4b274f5b4098fdc7f0854dc7
Encouse <54453362+Encouse@users.noreply.github.com>
2020-05-17 19:17:56 +0300

    Add files via upload

--- API/serializers.py
+++ API/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from . import models
 
+# Сериализатор класса BaseFilms со всеми указанными в классе полями
 class BaseFilmsSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.BaseFilms
         fields = '__all__'
+# -/ Александр Караваев
+
+
+# Сериализатор класса BaseFilms с ограниченным набором полей для краткого отображения
+class ShortBaseFilmsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.BaseFilms
+        fields = ['id','year','imdb_id','imdb_votes']
+# -/ Александр Караваев
--- API/urls.py
+++ API/urls.py
@@ -3,8 +3,24 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
+
+
+# В регулярной строке (first arg) прописаны названия paths в апи (r'films', r'bananas', etc.)
+
+# Все адреса роутера связаны по цепочке с - Views => Serializers => Models
+# Нужны для регистрации структур апи в роутере который в свою очередь отдает ее в urlpatterns
+
+# Отображение короткого описания фильмов в листе  (all fields) и длинного при детальном рассмотре фильма
 router.register(r'films', views.BaseFilmsViewSet)
+# -/ Александр Караваев
+
+# Отображение короткого описания фильмов в листе (restricted fields)
+router.register(r'brieffilms', views.ShortBaseFilmsViewSet)
+# -/ Александр Караваев
+
 
 urlpatterns = [
+    # Включение корневого роутера в список URLS
     re_path(r'^', include(router.urls))
+    # -/ Александр Караваев
 ]
