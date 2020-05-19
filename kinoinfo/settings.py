7548cad4e9f209b3828951ad923951e425704072
Encouse <54453362+Encouse@users.noreply.github.com>
2020-05-19 23:13:29 +0300

    CORS

--- kinoinfo/settings.py
+++ kinoinfo/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'kinoinfo.middleware.CORSMiddleware',
 ]
 
 ROOT_URLCONF = 'kinoinfo.urls'
