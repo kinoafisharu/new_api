13cda6d26c96a42a5d9a61dc288bce455e44923e
Иванов и Ко <32426965+kinoafisharu@users.noreply.github.com>
2020-05-20 17:52:01 +0300

    готовлю структурирование моделей
    пока добавил импорт из трёх файлов - два в приложении, универсальные - в корне

--- API/models.py
+++ API/models.py
@@ -6,6 +6,9 @@
 #   * Remov` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from models_univers import *
+from models_object import *
+from models_process import *
 
 
 
