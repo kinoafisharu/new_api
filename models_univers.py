e9e3c6f8601f05ccef70c7a2c5700eb0fb1e8d88
Иванов и Ко <32426965+kinoafisharu@users.noreply.github.com>
2020-05-22 17:18:28 +0300

    города

--- models_univers.py
+++ models_univers.py
@@ -1,1 +1,18 @@
 # Здесь будут модели универсальных объектов: годы, страны, города и др. 
+class BaseCity(models.Model):
+    phone_code = models.IntegerField(blank=True, null=True)
+    kid = models.BigIntegerField(blank=True, null=True)
+    country_id = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'base_city'
+
+
+class BaseCityName(models.Model):
+    city_id = models.IntegerField()
+    namecity_id = models.IntegerField()
+
+    class Meta:
+        db_table = 'base_city_name'
+        unique_together = (('city_id', 'namecity_id'),)
+
