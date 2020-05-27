fc561caa2aaacf1b764f82cb0110f4bb5f45dbd0
Encouse <54453362+Encouse@users.noreply.github.com>
2020-05-27 15:31:55 +0300

    Update settings.py

--- kinoinfo/settings.py
+++ kinoinfo/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base.apps.BaseConfig',
-    'rest_framework'
+    'rest_framework',
+    'djano_filters'
 
 ]
 
