136984762a5758ab173c10ee2e176185b6b6cd57
Encouse <54453362+Encouse@users.noreply.github.com>
2020-05-31 11:52:30 +0300

    Changed migration

--- base/migrations/0002_likes_filmobject.py
+++ base/migrations/0002_likes_filmobject.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0006_remove_likes_filmobject'),
+        ('base', '0001_initial'),
     ]
 
     operations = [
