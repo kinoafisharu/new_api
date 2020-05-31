406dcd84191bfb628c02c4d2029df9f9b1daa65f
Encouse <54453362+Encouse@users.noreply.github.com>
2020-05-31 13:03:48 +0300

    made likes COLUMN evaluation NOT NULLABLE

--- base/migrations/0005_auto_20200531_1001.py
+++ base/migrations/0005_auto_20200531_1001.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0008_auto_20200531_0835'),
+        ('base', '0004_auto_20200531_0835'),
     ]
 
     operations = [
