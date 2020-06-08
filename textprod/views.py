daad2029a8e418843a816d67c2a2d95fd80d2d50
Encouse <54453362+Encouse@users.noreply.github.com>
2020-06-08 18:41:16 +0300

    Метод getval, новая пагинация

--- textprod/views.py
+++ textprod/views.py
@@ -3,7 +3,8 @@ from rest_framework import viewsets
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, CursorPagination
+from base.pagination import FivePagination
 from rest_framework.decorators import action
 from base import serializers_helper
 from . import serializers
@@ -12,13 +13,6 @@ import logging
 
 # Классы отображения сериализорваных данных совмещают в себе list, retrieve, update, delete
 # Можно задавать дополнительные действия по запросам с помощью декоратора @action()
-
-#Класс пагинации по 5 обьектов на страницу, перенести в base/pagination.py
-class FivePagination(PageNumberPagination):
-    page_size = 5
-    page_size_query_param = 'page_size'
-    max_page_size = 50
-    
 
 logger = logging.getLogger(__name__)
 
@@ -30,11 +24,13 @@ class StoriesViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.StoriesSerializer
     pagination_class = FivePagination
 
+
     # Метод листинга с пагинацией, ограничение по полям
     def list(self, request):
         queryset = self.get_queryset()
         page = self.paginate_queryset(queryset)
         serializer = serializers.StoriesSerializer(page, many = True, fields = ('id','title','text', 'dtime','author', 'views',))
+        
         return self.get_paginated_response(serializer.data)
     # Метод сортировки историй по любому полю GET запросом и ограничение по выводу от 1 до 50
     # Параметр ?amount ограничивает выводимое количествo историй
@@ -45,5 +41,13 @@ class StoriesViewSet(viewsets.ModelViewSet):
         queryset = self.get_queryset().order_by(request.query_params['by'])
         page = self.paginate_queryset(queryset)
         serializer = self.get_serializer(page, queryset, many = True)
+        return Response(serializer.data)
+
+    # Метод принимает значение в виде названий полей модели, отдает результат в апи с обозначеными полями
+    @action(detail = False)
+    def getval(self, request):
+        queryset = self.get_queryset().values(request.query_params['values'])
+        page = self.paginate_queryset(queryset)
+        serializer = self.get_serializer(page, queryset, many = True, fields = request.query_params['values'])
         return Response(serializer.data)
 # -/ Александр Караваев
