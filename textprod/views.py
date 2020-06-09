e451a550577c12041839bc90b4e4bca3b37230da
Encouse <54453362+Encouse@users.noreply.github.com>
2020-06-09 17:41:51 +0300

    Метод сортировки с динамическими полями

--- textprod/views.py
+++ textprod/views.py
@@ -8,6 +8,7 @@ from rest_framework import filters
 from base.pagination import FivePagination
 from rest_framework.decorators import action
 from base import serializers_helper
+from base import filters as basefilters
 from . import serializers
 from base import models
 import logging
@@ -21,7 +22,7 @@ logger = logging.getLogger(__name__)
 Представление, отображающее листинг историй
 '''
 class StoriesViewSet(viewsets.ModelViewSet):
-    queryset = models.News.objects.filter(subdomain = 'memoirs').order_by('-id')
+    queryset = models.News.objects.filter(subdomain = 'memoirs')
     serializer_class = serializers.StoriesSerializer
     pagination_class = FivePagination
     filter_backends = [filters.OrderingFilter]
@@ -38,19 +39,23 @@ class StoriesViewSet(viewsets.ModelViewSet):
     # Параметр ?by определяет тип сортировки
     # (принимает стандартные аргументы функции order_by django, пример -dtime (убывание по дате))
     @action(detail = False)
-    def orderby(self, request):
-        queryset = self.get_queryset().order_by(request.query_params['by'])
+    def getval(self, request):
+        r = request.query_params.get('values', None)
+        sort = request.query_params.get('ordering', None)
+        data = r.split(',')
+        queryset = models.News.objects.all().order_by(str(sort))
         page = self.paginate_queryset(queryset)
-        serializer = self.get_serializer(page, queryset, many = True)
+        serializer = serializers.StoriesSerializer(page, many = True, fields = (data))
         return self.get_paginated_response(serializer.data)
 
     # Метод принимает значение в виде названий полей модели, отдает результат в апи с обозначеными полями
     @action(detail = False)
     def getval(self, request):
         r = request.query_params.get('values', None)
+        order = request.query_params.get('ordering', None)
         data = r.split(',')
-        queryset = self.get_queryset()
+        queryset = models.News.objects.all().order_by(str(order))
         page = self.paginate_queryset(queryset)
-        serializer = serializers.NewsSerializer(page, many = True, fields = (data))
+        serializer = serializers.StoriesSerializer(page, many = True, fields = (data))
         return self.get_paginated_response(serializer.data)
 # -/ Александр Караваев
