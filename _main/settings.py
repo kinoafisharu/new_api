fc4044607a8c01de060e84c7a13c2b1d7f4d3a19
Encouse <54453362+Encouse@users.noreply.github.com>
2020-06-09 14:58:02 +0300

    filter

--- _main/settings.py
+++ _main/settings.py
@@ -54,7 +54,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50
+    'PAGE_SIZE': 50,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.OrderingFilter',],
 }
 
 MIDDLEWARE = [
