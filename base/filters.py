1d313da626d79f24259b83e4a91f746c965c170a
Encouse <54453362+Encouse@users.noreply.github.com>
2020-06-16 17:22:17 +0300

    datetime

--- base/filters.py
+++ base/filters.py
@@ -1,15 +1,32 @@
 from rest_framework import filters
 from django.db.models import F, OuterRef, Subquery
+from django.utils.timezone import timedelta
+import datetime
+from django_filters import rest_framework as django_filters
 
 
 # Фильтрует обьекты ителлектуальной собственности,
 # не соответствующие набору обязательных полей
 # В классе нужно указать лист полей necessary_fields
 class NecessaryFieldsAssurance(filters.BaseFilterBackend):
+    def get_fields(self, view, request):
+        getattr(view, 'necessary_fields', None)
     def filter_queryset(self, request, queryset, view):
-        fields = self.necessary_fields
-        queryset.filter()
+        fields = self.get_fields(view, request)
         return queryset.filter(owner=request.user)
+
+class DateTimeFilter(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        date = request.query_params.get('datetime', None)
+        if date:
+            date = date.split(',')
+            if date[0] == 'laterbackfromnow':
+                date = datetime.datetime.now() - datetime.timedelta(int(date[1]))
+                return queryset.filter(release__release__gt = date)
+        return queryset
+
+
+
 
 # Класс сортировщика с возможностью фильтрования по query expression
 # Сортирует значения по нескольким или одному параметрам в порядке убывания или возрастания
