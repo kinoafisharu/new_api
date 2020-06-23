0836c0d38cc12534de2c3c0139134c09bb80e0ce
Encouse <54453362+Encouse@users.noreply.github.com>
2020-06-23 20:32:45 +0300

    Точечный парсинг на bs4

--- parsing/views.py
+++ parsing/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-import os
-from scrapyd.config import Config
 from urllib.parse import urlparse
+from django.core import exceptions
+import logging
+import random
+import os
+from scrapy_crawl.scrapy_crawl.spiders.imdb_crawler import ImdbSpider
 from base import models
 from uuid import uuid4
 from kinoinfo import serializers as kserializers
@@ -11,31 +14,43 @@ from django.core.exceptions import ValidationError
 from django.views.decorators.http import require_POST, require_http_methods
 from django.shortcuts import render
 from scrapyd_api import ScrapydAPI
-from base.models import Films
+from scrapy.crawler import CrawlerProcess
+from bs4 import BeautifulSoup
+import requests as req
 
-
-scrapyd = ScrapydAPI('http://127.0.0.1:6800/')
+logger = logging.getLogger(__name__)
 
 class FilmParseView(APIView):
     def post(self, request):
         imdb_id = request.data.get('imdb_id', None)
-        if imdb_id:
-            task = scrapyd.schedule('default', 'imdb_crawl', imdb_id = imdb_id)
-            return Response({'task_id': task, 'status': 'started'})
+        resp = req.get('https://www.imdb.com/title/tt{0}/'.format(imdb_id))
+        soup = BeautifulSoup(resp.text, 'lxml')
+        title = soup.find('div', {'class': 'title_wrapper'})
+        title = title.find('h1')
+        title.span.decompose()
+        title = title.get_text().strip()
+        try:
+            film = models.Films.objects.get(imdb_id = imdb_id)
+            name = models.NameFilms.objects.create(name = title, status = 1)
+            film.name.add(name)
+            film.save()
+            logger.info(film.name.all())
+        except exceptions.ObjectDoesNotExist as e:
+            name = models.NameFilms.objects.create(name = title, status = 1)
+            film = models.Films.objects.create(id = random.randint(1000000,9999999))
+            film.imdb_id = imdb_id
+            film.name.add(name)
+            film.save()
+            logger.info(film.name.all())
+        except exceptions.MultipleObjectsReturned as e:
+            print(e)
+        return Response({'task_id': '1', 'name': str(title)})
+
     def get(self, request):
-        task_id = request.query_params.get('task_id', None)
         imdb_id = request.query_params.get('imdb_id', None)
-        if not task_id:
-            return Response({"errors": ['Missing task ID']})
-        status = scrapyd.job_status('default', task_id)
-        if not status:
-            return Response({'status': 'Wrong id'})
-        if status == 'finished':
-            try:
-                item = models.Films.objects.get(imdb_id = str(imdb_id))
-                serializer = kserializers.FilmsSerializer(item)
-                return Response({'data': serializer.data})
-            except Exception as e:
-                return Response({'error': str(e)})
-        else:
-                return Response({'status': status})
+        try:
+            item = models.Films.objects.get(imdb_id = str(imdb_id))
+            serializer = kserializers.FilmsSerializer(item)
+            return Response({'data': serializer.data})
+        except Exception as e:
+            return Response({'error': str(e)})
