157fa307d8c5cc3fa08b3d95a8ae0c8e072c5960
Encouse <54453362+Encouse@users.noreply.github.com>
2020-06-24 19:58:52 +0300

    Парсинг

--- kinoinfo/serializers.py
+++ kinoinfo/serializers.py
@@ -1,41 +1,61 @@
+
 from rest_framework import serializers
-from base import models
-from base.serializers_dic import *
-from base.mixins import *
-from base import serializers as base_serializers
-from base.serializers_helper import *
-from base import serializer_fields
-from . import serializer_fields as local_fields
+from . import models
+from .serializers_dic import *
+from .mixins import *
+from collections import OrderedDict
+from .serializers_helper import *
+from . import serializer_fields
 
 
-# -/ Александр Караваев =>
-'''
-Здесь определяются классы сериализаторов для конкретных моделей
-Необходим для преобразования данных из бд, привязанной к ОРМ джанго в нативные типы данных
-и дальнейшего их рендера в апи в формате JSON
-'''
-
-# Сериализатор класса Films со всеми указанными в классе полями
-'''
-Используется в детальном отображении фильма в апи, описан в файлах: views.py
-Поддерживает динамическое изменение полей при передаче аргумента fields (кортеж с полями)
-'''
-class FilmsSerializer(DynamicFieldsModelSerializer):
-    votes = LikeSerializer()
-    sourcefilms_set = SourceFilmsSerializer(many = True, fields = ('name','top250_set'))
-    persons = serializer_fields.PersonField(objectattributes = ['name_main'])
-    name = FilmNameSerializer(many = True, fields = ('name',))
-    release = FilmReleaseSerializer(many = True, fields = ('release', 'note'))
-    country = CountrySerializer(many = True, fields = ('name',))
-    genre = FilmGenreSerializer(many = True, fields = ('name',))
-    production = ProductionSerializer(many = True)
-    distributor = DistributorSerializer(many = True)
-    images = ImageSerializer(many = True, fields = ('file',))
-    budget = FilmBudgetSerializer(many = False, fields = ('budget',))
-
+class PersonSerializer(DynamicFieldsModelSerializer):
+    name = NamePersonSerializer(many = True, fields = ('name',))
+    country = CountrySerializer(many = False, fields = ('name',))
     class Meta:
-        model = models.Films
+        model = models.Person
         fields = '__all__'
 
 
-# -/ Александр Караваев
+class ProfileSerializer(DynamicFieldsModelSerializer):
+    person = PersonSerializer(many = False, fields = ('name',))
+    class Meta:
+        model = models.Profile
+        fields = '__all__'
+
+# =================================================================================================
+
+# Александр Караваев ==>
+# Вспомогательный класс сериализации для парсинга данных с asteroid (Когда данные будут перенесены, весь код под чертой выше к удалению!)
+class AsteroidFilmSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    original_title = serializers.CharField()
+    country = serializers.IntegerField()
+    year = serializers.CharField()
+    company = serializers.IntegerField()
+    genre1 = serializers.IntegerField()
+    genre2 = serializers.IntegerField()
+    genre3 = serializers.IntegerField()
+    site = serializers.CharField()
+    director1 = serializers.IntegerField()
+    director2 = serializers.IntegerField()
+    director3 = serializers.IntegerField()
+    imdb = serializers.CharField()
+    imdb_votes = serializers.IntegerField()
+    actor1 = serializers.IntegerField()
+    actor2 = serializers.IntegerField()
+    actor3 = serializers.IntegerField()
+    actor4 = serializers.IntegerField()
+    actor5 = serializers.IntegerField()
+    actor6 = serializers.IntegerField()
+    runtime = serializers.CharField()
+    limits = serializers.CharField()
+    prokat1 = serializers.IntegerField()
+    prokat2 = serializers.IntegerField()
+    date = serializers.DateTimeField()
+    description = serializers.CharField()
+    comment = serializers.CharField()
+    country2 = serializers.IntegerField()
+    trailers = serializers.CharField()
+    idalldvd = serializers.IntegerField()
+    datelastupd = serializers.DateTimeField()
