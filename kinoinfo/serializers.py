d835458db25db9658508d0ef43c2288c667847ad
Encouse <54453362+Encouse@users.noreply.github.com>
2020-06-25 16:23:57 +0300

    Парсиг постеров, модульность

--- kinoinfo/serializers.py
+++ kinoinfo/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+from django.core import exceptions
 from base import models
+from base.func import save_film_namedata
 from base.serializers_dic import *
 from base.mixins import *
 from base import serializers as base_serializers
@@ -20,26 +22,49 @@ from . import serializer_fields as local_fields
 Используется в детальном отображении фильма в апи, описан в файлах: views.py
 Поддерживает динамическое изменение полей при передаче аргумента fields (кортеж с полями)
 '''
+
+
+
 class FilmsSerializer(DynamicFieldsModelSerializer):
     votes = LikeSerializer()
-    sourcefilms_set = SourceFilmsSerializer(many = True, fields = ('name','top250_set'))
+    sourcefilms_set = SourceFilmsSerializer(many = True, repfields = ('name','top250_set'))
     persons = serializer_fields.PersonField(objectattributes = ['name_main'])
-    name = FilmNameSerializer(many = True, fields = ('name',))
-    release = FilmReleaseSerializer(many = True, fields = ('release', 'note'))
-    country = CountrySerializer(many = True, fields = ('name',))
-    genre = FilmGenreSerializer(many = True, fields = ('name',))
+    name = FilmNameSerializer(many = True, repfields = ('name',))
+    release = FilmReleaseSerializer(many = True, repfields = ('release', 'note'))
+    country = CountrySerializer(many = True, repfields = ('name',))
+    genre = FilmGenreSerializer(many = True, repfields = ('name',))
     production = ProductionSerializer(many = True)
     distributor = DistributorSerializer(many = True)
-    images = ImageSerializer(many = True, fields = ('file',))
-    budget = FilmBudgetSerializer(many = False, fields = ('budget',))
+    images = ImageSerializer(many = True, repfields = ('file',))
+    budget = FilmBudgetSerializer(many = False, repfields = ('budget',))
 
+    # Метод создает обьект если его не существует
     def create(self, validated_data):
-        return models.Films.objects.create(**validated_data)
+        names = validated_data.pop('name', None)
+        film = models.Films.objects.create(**validated_data)
+        if names:
+            for name in names:
+                obj = self.get_first_or_create(model = models.NameFilms, data = name)
+                print(obj)
+                if obj:
+                    film.name.add(obj)
+                    print(f'One object {obj} added to {film}')
+        return film
 
+    # Метод обновляет обьект если он существут
     def update(self, instance, validated_data):
+        names = validated_data.pop('name', None)
         instance.imdb_rate = validated_data.get('imdb_rate', instance.imdb_rate)
         instance.imdb_votes = validated_data.get('imdb_votes', instance.imdb_votes)
+        instance.imdb_id = validated_data.get('imdb_id', instance.imdb_id)
         instance.save()
+        if names:
+            for name in names:
+                obj = self.get_first_or_create(model = models.NameFilms, data = name)
+                print(obj)
+                if obj:
+                    instance.name.add(obj)
+                    print(f'One object {obj} added to {instance}')
         return instance
 
     class Meta:
