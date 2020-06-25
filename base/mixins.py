d9218a83e42ef6fcb852a8234991d24ec9c33b40
Encouse <54453362+Encouse@users.noreply.github.com>
2020-06-25 16:25:05 +0300

    Прасинг постеров, модульность

--- base/mixins.py
+++ base/mixins.py
@@ -1,4 +1,12 @@
 from rest_framework import serializers
+from collections import OrderedDict
+
+# Non-field imports, but public API
+from rest_framework.fields import (
+     SkipField
+)
+from rest_framework.relations import PKOnlyObject
+
 
 # Александр Караваев  ===>
 '''
@@ -13,6 +21,7 @@ class DynamicFieldsModelSerializer(serializers.ModelSerializer):
     def __init__(self, *args, **kwargs):
         # Don't pass the 'fields' arg up to the superclass
         fields = kwargs.pop('fields', None)
+        self.repfields = kwargs.pop('repfields', None)
 
         # Instantiate the superclass normally
         super(DynamicFieldsModelSerializer, self).__init__(*args, **kwargs)
@@ -23,4 +32,41 @@ class DynamicFieldsModelSerializer(serializers.ModelSerializer):
             existing = set(self.fields)
             for field_name in existing - allowed:
                 self.fields.pop(field_name)
+
+    def to_representation(self, instance):
+        ret = OrderedDict()
+        fields = self._readable_fields
+        allowed = set(self.repfields) if self.repfields else None
+
+
+        for field in fields:
+            try:
+                attribute = field.get_attribute(instance)
+            except SkipField:
+                continue
+
+            check_for_none = attribute.pk if isinstance(attribute, PKOnlyObject) else attribute
+            if check_for_none is None:
+                ret[field.field_name] = None
+            else:
+                ret[field.field_name] = field.to_representation(attribute)
+        retc = ret.copy()
+        if allowed:
+            for key in ret.keys():
+                if key not in allowed:
+                    del retc[key]
+        return retc
+
+    def get_first_or_create(self, model = None, data = None, **kwargs):
+        try:
+            getter_data = kwargs.pop('getter_data', None)
+            if getter_data:
+                obj = model.objects.get(**getter_data)
+            else:
+                obj = model.objects.get(**data)
+        except exceptions.ObjectDoesNotExist as e:
+            obj = model.objects.create(**data)
+        except exceptions.MultipleObjectsReturned:
+            obj = model.objects.filter(**data)[0]
+        return obj if obj else None
 #====================================================================================
