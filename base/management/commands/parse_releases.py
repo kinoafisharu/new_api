eef821cad2c15cc1ca8a4ca16f07ea45cdf11829
Encouse <54453362+Encouse@users.noreply.github.com>
2020-06-27 18:16:58 +0300

    fix

--- base/management/commands/parse_releases.py
+++ base/management/commands/parse_releases.py
@@ -10,25 +10,26 @@ from base import models
 # Парсит даты релизов с kinoinfo
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        urls = [{'url': f'http://kinoinfo.ru/film/{i}/', 'id': i} for i in range(35000, 42000)]
-        for url in urls:
-            resp = req.get(url['url'])
-            soup = BeautifulSoup(resp.text, 'lxml')
-            parser = parsers.KinoinfoParser()
-            releasedate = parser.parse_release(soup)
-            if releasedate:
-                date = datetime.strptime(str(releasedate), '%d %B %Y')
-                date = datetime.strftime(date, '%Y %m %d').replace(' ', '-')
-                print(date)
-                serializer = serializers_helper.FilmReleaseSerializer(data = {'release': date})
-                if serializer.is_valid():
-                    relobj = serializer.save()
-                    try:
-                        film = models.Films.objects.filter(kid = url['id'])[0]
-                        if film.release.exists():
-                            for rel in film.release.all():
-                                rel.delete()
-                        film.release.add(relobj)
-                        self.stdout.write(f'Added on release {relobj.release} to the film {film}')
-                    except Exception as e:
-                        self.stdout.write(str(e))
+        with open('kids.txt', 'r') as f:
+            urls = [{'url': f'http://kinoinfo.ru/film/{i}/', 'id': i} for i in f.read.split(',')]
+            for url in urls:
+                resp = req.get(url['url'])
+                soup = BeautifulSoup(resp.text, 'lxml')
+                parser = parsers.KinoinfoParser()
+                releasedate = parser.parse_release(soup)
+                if releasedate:
+                    date = datetime.strptime(str(releasedate), '%d %B %Y')
+                    date = datetime.strftime(date, '%Y %m %d').replace(' ', '-')
+                    print(date)
+                    serializer = serializers_helper.FilmReleaseSerializer(data = {'release': date})
+                    if serializer.is_valid():
+                        relobj = serializer.save()
+                        try:
+                            film = models.Films.objects.filter(kid = url['id'])[0]
+                            if film.release.exists():
+                                for rel in film.release.all():
+                                    rel.delete()
+                            film.release.add(relobj)
+                            self.stdout.write(f'Added on release {relobj.release} to the film {film}')
+                        except Exception as e:
+                            self.stdout.write(str(e))
