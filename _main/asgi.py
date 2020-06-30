2552d8d9c84df4313065f1add3bf3a9381fd0df0
Encouse <54453362+Encouse@users.noreply.github.com>
2020-06-30 19:38:44 +0300

    fix

--- _main/asgi.py
+++ _main/asgi.py
@@ -1,16 +1,5 @@
-"""
-ASGI config for kinoinfo project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
-"""
-
 import os
+import channels.asgi
 
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kinoinfo.settings')
-
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "_main.settings")
+channel_layer = channels.asgi.get_channel_layer()
